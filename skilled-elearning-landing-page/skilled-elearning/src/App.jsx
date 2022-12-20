ea2e4b1f7bf3296b0b430bce4e12f3b3cdbdd821
jmadrigaladan <jorgemadrigaladan@gmail.com>
2022-12-19 17:30:10 -0800

    working on configuring styles.js file

---
+++ skilled-elearning-landing-page/skilled-elearning/src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import styles from "./styles";
+
+const App = () => (
+  <div>
+    <div>
+      <h1 className={`${styles.heading}`}>Hello World</h1>
+      {/* NavBar */}
+    </div>
+
+    <div>{/* Hero Section */}</div>
+
+    <div>{/* Serivices Section */}</div>
+
+    <div>{/* Footer */}</div>
+  </div>
+);
+
+export default App;
---
+++ skilled-elearning-landing-page/skilled-elearning/src/styles.js
@@ -0,0 +1,6 @@
+const styles = {
+  heading:
+    "font-jakartaSans text-[#13183F] font-extrabold text-[40px] leading-[50px] md:text-[56px] md:leading-[71px]",
+};
+
+export default styles;
